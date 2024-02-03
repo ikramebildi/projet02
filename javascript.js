5aa55ac449246d6835bb156492b2cb652f4bca75
ikramebildi <ikrambd99@gmail.com>
2024-02-03 13:12:29 +0100

    *projet02

---
+++ javascript.js
@@ -0,0 +1,77 @@
+//  les variables
+var name = "ikrame"
+var number = 23
+var age = 16
+
+console.log(name)
+console.log(23)
+console.log(age)
+
+// les operateurs
+
+var sum =20*6
+console.log(sum)
+var deez =16+14
+console.log(deez)
+
+// varname
+// varName
+// var_name
+
+varname = 23
+console.log (varname)
+
+var_name =23
+console.log(var_name)
+
+
+// *  les fonctions
+
+
+// ex 01
+var a =99
+var b =18
+
+function myFunction(){
+    var totale=a*b
+    console.log(totale)
+}
+
+
+myFunction()
+
+// ex 02
+var a =90
+var b =15
+
+function sumNumber(){
+    var totale = a/b+14
+    console.log(totale)
+}
+
+
+sumNumber()
+
+function sumAge(a,b){
+    console.log(a+b*7)
+
+}
+sumAge(20,13)
+sumAge(20,16)
+sumAge(20,100)
+sumAge(20,100)
+sumAge(20,100)
+
+function fullName( firstname,lastname){
+    console.log(firstname,lastname)
+}
+fullName("basma","basma")
+
+// array
+
+ var myArray = [2,3,5,5,9,10,11]
+ console.log(myArray)
+
+ var arrayString = ["ik","bd","ha","Ne","dg"]
+ console.log(arrayString)
+
